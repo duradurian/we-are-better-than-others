7937026905676777f80f157df2141016842e4bba
duradurian <113811889+duradurian@users.noreply.github.com>
2022-10-11 14:03:12 -0400

    Update pxt.json, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+def main():
+    input.button_is_pressed(Button.A)
+    radio.set_group(42)
+    radio.send_number(2)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
-})
+function main() {
+    input.buttonIsPressed(Button.A)
+    radio.setGroup(42)
+    radio.sendNumber(2)
+}
+
