4c0394bf72843ffaf13948c957ec58061598f349
duradurian <113811889+duradurian@users.noreply.github.com>
2022-10-13 23:40:11 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,33 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 2) {
+        basic.showIcon(IconNames.Square)
         motion.driveStraight(50)
         basic.pause(500)
         motion.stop()
+        basic.clearScreen()
     } else if (receivedNumber == 1) {
+        basic.showIcon(IconNames.SmallDiamond)
         motion.driveStraight(0)
         motion.stop()
+        basic.clearScreen()
     } else if (receivedNumber == 3) {
+        basic.showIcon(IconNames.SmallSquare)
         motion.turnLeft(40)
         basic.pause(500)
         motion.stop()
+        basic.clearScreen()
     } else if (receivedNumber == 4) {
+        basic.showIcon(IconNames.Target)
         motion.turnRight(40)
         basic.pause(500)
         motion.stop()
+        basic.clearScreen()
     } else {
         basic.showIcon(IconNames.No)
     }
 })
 radio.setGroup(42)
+radio.setTransmitPower(7)
 basic.showIcon(IconNames.Yes)
 basic.pause(500)
 basic.clearScreen()
